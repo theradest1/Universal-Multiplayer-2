45eabd8cbe3066039f1e6779f5fc9bda4b7e1ef9
TheRad1 <landon.bakken@gmail.com>
2024-01-19 16:26:20 -0600

    Added min TPS
    I also did a 3 player test. Everything worked perfectly except two things:
    - the TCP error forced me to restart a client (just once tho)
    - there is a small blip at the start where the other players are at the center (I think this is due to the initial transform set not working)

--- Assets/UM2/Object Sync/UM2_Object.cs
+++ Assets/UM2/Object Sync/UM2_Object.cs
@@ -9,14 +9,17 @@ public class UM2_Object : MonoBehaviour
     public GameObject prefab;
     [HideInInspector] public int objectID = -1;
 
-    [Range(1,64)]
     float pastTicksPerSecond = -1;
+    [Range(1,64)]
     public float ticksPerSecond;
     UM2_Sync sync;
 	UM2_Variables variables;
 
     public bool syncTransform = true;
     public bool optimizeTransoformSync = true;
+    [Range(0, 16)]
+    public float minTicksPerSecond;
+    float pastSyncTime = 0;
     bool pastSyncTransform = false;
     bool initialized = false;
 
@@ -51,23 +54,29 @@ public class UM2_Object : MonoBehaviour
                     updateTransform();
                 }
             }
-
-            if(pastTicksPerSecond != ticksPerSecond){
-                pastTicksPerSecond = ticksPerSecond;
-                sync.updateTPS(objectID, ticksPerSecond);
-            }
         }
     }
 
     public async void updateTransform(bool forced = false){
         if(this != null && syncTransform){
-            //this if statement only goes through if transform isnt changed
-            //(not including scale)
-            //(only if optimizeTransoformSync is true)
-            if(pastSyncedPos != transform.position || pastSyncedRot != transform.rotation || !optimizeTransoformSync || forced){
+            if(pastTicksPerSecond != ticksPerSecond){
+                pastTicksPerSecond = ticksPerSecond;
+                sync.updateTPS(objectID, ticksPerSecond);
+            }
+            
+            bool transformChanged = pastSyncedPos != transform.position || pastSyncedRot != transform.rotation;
+            
+            bool isMinUpdateRate = false;
+            if(minTicksPerSecond > 0){
+                float minTPSTime = 1/minTicksPerSecond;
+                isMinUpdateRate = minTicksPerSecond <= Time.time - pastSyncTime;
+            }
+            
+            if(transformChanged || !optimizeTransoformSync || forced || isMinUpdateRate){
                 sync.updateObject(objectID, transform.position, transform.rotation);
                 pastSyncedPos = transform.position;
                 pastSyncedRot = transform.rotation;
+                pastSyncTime = Time.time;
             }
 
             await Task.Delay((int)(1/ticksPerSecond*1000));
