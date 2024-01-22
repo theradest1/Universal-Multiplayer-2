ab3393ae72c70332c18bf3c90b67b71315a8eeae
TheRad1 <landon.bakken@gmail.com>
2024-01-22 00:45:43 -0600

    cleaned up code a bit

--- Assets/UM2_Methods.cs
+++ Assets/UM2_Methods.cs
@@ -18,31 +18,32 @@ public class UM2_Methods : MonoBehaviour
     */
 
     public void callGlobalMethod(String methodName, object[] perameters = null){
-        //I know this is kind of a terrible implementation
-        //that breaks some pretty set-in-stone-rules of programming
-        //but I dont really care - its all for ease of access
-
-        //get all current enabled game object in the scene
+        //get all enabled game object in the scene
         GameObject[] allGameObjects = GameObject.FindObjectsOfType<GameObject>();
+
         foreach (GameObject gameObject in allGameObjects) {
+
             //get all monobehaviours on the game object
             MonoBehaviour[] scripts = gameObject.GetComponents<MonoBehaviour>();
             foreach(MonoBehaviour script in scripts){
-                //get method info (makes it possible to call private functions)
+
+                //get method info (makes it possible to call private methods)
                 MethodInfo methodInfo = script.GetType().GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+                //if the method exists in the Monobehaviour
                 if(methodInfo != null){
-                    //run the method
+                    //try-catch for if parameters don't match up
                     try
                     {
+                        //run the method with optional parameters
                         methodInfo.Invoke(script, perameters);
                     }
                     catch (Exception e)
                     {
-                        Debug.LogError("Error when calling global UM2 function: " + e);
+                        Debug.LogError("Error when calling global method: " + e);
                     }
                 }
             }
-            //gameObject.SendMessage(methodName, SendMessageOptions.DontRequireReceiver);
         }
     }
 
