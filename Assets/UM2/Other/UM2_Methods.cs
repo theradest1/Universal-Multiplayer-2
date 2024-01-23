94f05a4b0eb385acf7f3f1be87d5da5fb2fb76c3
TheRad1 <18326@verona.k12.wi.us>
2024-01-22 19:23:52 -0600

    Update UM2_Methods.cs

--- Assets/UM2/Other/UM2_Methods.cs
+++ Assets/UM2/Other/UM2_Methods.cs
@@ -6,18 +6,24 @@ using System.Reflection;
 
 public class UM2_Methods : MonoBehaviourUM2
 {
+    /*
+    this script keeps track of all of the scripts 
+    inhariting the methods of MonoBehaviourUM2
+    so they can be called efficiently
+    
+    look in MonoBehaviourUM2 for the methods
+    */
 
-    public static List<MonoBehaviourUM2> subscribedClasses = new List<MonoBehaviourUM2>();
+    public static List<MonoBehaviourUM2> subscribedScripts = new List<MonoBehaviourUM2>();
 
-    //look in MonoBehaviourUM2 for used methods
 
     public static void addToCallback(MonoBehaviourUM2 classToAdd){
-        subscribedClasses.Add(classToAdd);
+        subscribedScripts.Add(classToAdd);
     }
 
     public static void removeFromCallback(MonoBehaviourUM2 classToRemove){
-        if(subscribedClasses.Contains(classToRemove)){
-            subscribedClasses.Remove(classToRemove);
+        if(subscribedScripts.Contains(classToRemove)){
+            subscribedScripts.Remove(classToRemove);
         }
         else{
             Debug.LogWarning("Tried to remove class from subscription list that wasnt there");
@@ -25,7 +31,7 @@ public class UM2_Methods : MonoBehaviourUM2
     }
 
     public static void callGlobalMethod(String methodName, object[] perameters = null){
-        foreach(MonoBehaviourUM2 subscribedClass in subscribedClasses){
+        foreach(MonoBehaviourUM2 subscribedClass in subscribedScripts){
             MethodInfo methodInfo = subscribedClass.GetType().GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
             methodInfo.Invoke(subscribedClass, perameters);
         }
